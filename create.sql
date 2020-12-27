d0300491fd22d3048ac68ed5939ff84f9953f448
Jeyhun Gurbanov <jeyhun.j.gurbanov@gmail.com>
2020-12-27 10:45:24 -0700

    Add clients table

--- create.sql
+++ create.sql
@@ -9,3 +9,11 @@ CREATE TABLE admins (
 CREATE TABLE auth (
     token_id       TEXT  PRIMARY KEY
 );
+
+CREATE TABLE clients (
+    id          SERIAL  PRIMARY KEY,
+    first_name  TEXT    NOT NULL,
+    last_name   TEXT    NOT NULL,
+    email       TEXT    NOT NULL,
+    pwd_hash    TEXT    NOT NULL
+);
